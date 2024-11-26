5539c3e8ba4d1d7c8ac2750f33f3777497297391
MostOriginalIGN <winhtetlin0001@gmail.com>
2024-11-26 00:49:55 -0800

    sliding bar thing

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import React, { FormEvent, useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { signIn, useSession } from 'next-auth/react';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { useToast } from "@/hooks/use-toast";
-import { z } from 'zod';
+import React, {FormEvent, useEffect, useState} from 'react';
+import {useRouter} from 'next/navigation';
+import {signIn, useSession} from 'next-auth/react';
+import {Input} from '@/components/ui/input';
+import {Button} from '@/components/ui/button';
+import {Label} from '@/components/ui/label';
+import {useToast} from "@/hooks/use-toast";
+import {z} from 'zod';
 import bcrypt from "bcryptjs";
 
 const signUpSchema = z.object({
@@ -148,27 +148,26 @@ export default function Auth() {
                     </p>
                 </div>
 
-                <div className="flex border-b border-gray-200 mb-6">
+                <div className="relative flex mb-6">
                     <button
                         onClick={() => setActiveTab('signIn')}
-                        className={`w-1/2 py-2 text-center font-medium ${
-                            activeTab === 'signIn'
-                                ? 'text-indigo-600 border-b-2 border-indigo-600'
-                                : 'text-gray-600'
-                        }`}
+                        className={`w-1/2 py-2 text-center font-medium focus:outline-none ${
+    activeTab === 'signIn' ? 'text-indigo-600' : 'text-gray-600'}`}
                     >
                         Sign In
                     </button>
                     <button
                         onClick={() => setActiveTab('signUp')}
-                        className={`w-1/2 py-2 text-center font-medium ${
-                            activeTab === 'signUp'
-                                ? 'text-indigo-600 border-b-2 border-indigo-600'
-                                : 'text-gray-600'
-                        }`}
+                        className={`w-1/2 py-2 text-center font-medium focus:outline-none ${
+                            activeTab === 'signUp' ? 'text-indigo-600' : 'text-gray-600'}`}
                     >
                         Sign Up
                     </button>
+                    <span
+                        className={`absolute bottom-0 left-0 w-1/2 h-0.5 bg-indigo-600 transition-transform duration-300 ${
+                            activeTab === 'signIn' ? 'transform translate-x-0' : 'transform translate-x-full'
+                        }`}
+                    />
                 </div>
 
                 {activeTab === 'signIn' ? (
