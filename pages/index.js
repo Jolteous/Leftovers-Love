6d295adf2ce7efe17de028b3ff4239edeacadc7b
Jahaanshah Sheikh <wirelesskhurram@outlook.com>
2024-11-25 16:05:35 -0800

    Initialization

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Leftovers Love</title>
+        <meta name="description" content="Reduce food waste by suggesting recipes for leftovers" />
+      </Head>
+      <main>
+        <h1>Welcome to Leftovers Love</h1>
+        <p>Reduce food waste by suggesting recipes for leftovers.</p>
+      </main>
+    </div>
+  );
+}
